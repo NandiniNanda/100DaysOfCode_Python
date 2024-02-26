914f55f66ff492e45d33fcc3dcdb9a6b4071d08c
Nandini Nanda <100901728+NandiniNanda@users.noreply.github.com>
2024-02-26 23:09:53 +0530

    Add files via upload

---
+++ My_Learning/Day_37/MinStack.py
@@ -0,0 +1,38 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            popped = self.stack.pop()
+            if popped == self.min_stack[-1]:
+                self.min_stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+
+
+# Example usage:
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print(minStack.getMin())  # Output: -3
+minStack.pop()
+print(minStack.top())     # Output: 0
+print(minStack.getMin())  # Output: -2
